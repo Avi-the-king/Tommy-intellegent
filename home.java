298d6f23a9e996ab5c84bad250dad229bc3fa2fd
Avi-the-king <hsnaskna@gmail.com>
2017-04-13 12:22:31 +0530

    Add files via upload

---
+++ home.java
@@ -0,0 +1,16 @@
+//program to develop AI.
+import os.java.awt;*
+import maths.java.awt;*
+import sqlite3.java.awt;*
+import secrets.java.awt;*
+
+private class home
+{
+     private static void main (secrets.user) (strings[]args)
+     {
+      [('import maths.functions') ('import sqlite3.functions')
+      (system.out.println('my name is Tommy, and I am telling you I am very intellegent.Well, hello (secrets.username)'))]
+      [(system.out.println('well, if you are intellegent or fool I can help you in maths,physics,chemistry,biology,and in translating words from one language to another,i can also make videos, record sounds even help you with searching.')
+      "for translating take help of google translate","for searching take help from google, yahoo, bing, quora, stack exchange.","for making videos take help of camera if available.","for recording sounds take help of mic if avalaible","for calculation take help of google calculator.","for for voice output take help of speaker if available.","for date and time take help of google","for scanning viruses take help of mcafee online service"]
+      }
+ }
